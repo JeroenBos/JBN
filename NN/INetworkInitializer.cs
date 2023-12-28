9ca409b0c7f07c951c869438a02ceff51a63a746
Jeroen Bos <jer123bos@gmail.com>
2023-12-28 17:32:55 +0100

    Create files for `NetworkInitializer`

---
+++ NN/INetworkInitializer.cs
@@ -0,0 +1,25 @@
+﻿using JBSnorro.NN.Internals;
+
+namespace JBSnorro.NN;
+
+public interface INetworkInitializer
+{
+    internal const int INITIALIZATION_TIME = 0;
+    public static INetworkInitializer CreateRandom(int seed)
+    {
+        return CreateRandom(new Random(seed));
+    }
+    public static INetworkInitializer CreateRandom(Random random)
+    {
+        return new RandomNetworkInitializer(random);
+    }
+    public static INetworkInitializer CreateUniformActivator()
+    {
+        return new UniformNetworkInitializer();
+    }
+
+    /// <summary>
+    /// Gets the input axons to activate when on start.
+    /// </summary>
+    internal void Activate(IReadOnlyList<Axon> axons, IMachine machine);
+}
---
+++ NN/Internals/NetworkInitializers.cs
@@ -0,0 +1,37 @@
+﻿namespace JBSnorro.NN.Internals;
+
+internal sealed class RandomNetworkInitializer : INetworkInitializer
+{
+    public Random Random { get; }
+
+    public RandomNetworkInitializer(Random random)
+    {
+        this.Random = random;
+    }
+
+    public void Activate(IReadOnlyList<Axon> axons, IMachine machine)
+    {
+        foreach (var axon in axons)
+        {
+            bool activate = Random.Next(2) == 0;
+            if (activate)
+            {
+                machine.AddEmitAction(INetworkInitializer.INITIALIZATION_TIME, axon);
+            }
+        }
+    }
+}
+
+/// <summary>
+/// Activates all input axons.
+/// </summary>
+internal sealed class UniformNetworkInitializer : INetworkInitializer
+{
+    public void Activate(IReadOnlyList<Axon> axons, IMachine machine)
+    {
+        foreach (var axon in axons)
+        {
+            machine.AddEmitAction(INetworkInitializer.INITIALIZATION_TIME, axon);
+        }
+    }
+}
