8ceb3aea13629797c8a56c0b5c2604b3b428d3d2
Jeroen Bos <jer123bos@gmail.com>
2024-02-02 21:49:58 +0100

    Play with test that creates logical AND gate

---
+++ tests/ManuallyConstructedNetworkTests.cs
@@ -0,0 +1,81 @@
+﻿using JBSnorro.NN;
+using System.Diagnostics;
+
+namespace Tests.JBSnorro.NN;
+
+public class AND
+{
+    class NetworkFactory : INetworkFactory
+    {
+        public IReadOnlyList<INeuronType> NeuronTypes { get; } = Enumerable.Repeat(INeuronType.NoRetentionNeuronType, /*neuron count: */3).ToArray();
+        public int OutputCount => 1;
+        public INetworkFeeder InputPrimer { get; }
+
+        public NetworkFactory(INetworkFeeder inputPrimer)
+        {
+            this.InputPrimer = inputPrimer;
+        }
+
+        public IAxonInitialization? GetAxonConnection(int neuronFromIndex, int neuronToIndex)
+        {
+            switch ((neuronFromIndex, neuronToIndex))
+            {
+                case (IAxonType.FROM_INPUT, 0):
+                case (IAxonType.FROM_INPUT, 1):
+                    return IAxonInitialization.Create(length: 1, new float[] { 1 }, IAxonType.Input);
+                case (0, 2):
+                case (1, 2):
+                    return IAxonInitialization.Create(length: 1, new float[] { 0.5f }, UnchangingAxonType.Instance);
+                case (0, 0):
+                case (1, 1):
+                case (2, 2):
+                case (2, 1):
+                case (2, 0):
+                case (0, 1):
+                case (1, 0):
+                case (IAxonType.FROM_INPUT, 2):
+                    return null;
+                default: throw new UnreachableException();
+            }
+        }
+    }
+    private (IMachine Machine, INetwork Network) Construct(bool input1, bool input2, int maxTime = 2)
+    {
+        var inputSequence = new[] { new bool[] { input1, input2 } };
+
+        INetworkFactory factory = new NetworkFactory(INetworkFeeder.CreateDeterministicFeeder(inputSequence));
+        return factory.Create(maxTime);
+    }
+    [Fact]
+    public void True_and_true_gives_true()
+    {
+        var machine = Construct(true, true).Machine;
+        var output = machine.Run();
+
+        Assert.True(output[0] > 0);
+    }
+    [Fact]
+    public void False_and_true_gives_false()
+    {
+        var machine = Construct(false, true).Machine;
+        var output = machine.Run();
+
+        Assert.True(output[0] == 0.5);
+    }
+    [Fact]
+    public void True_and_false_gives_false()
+    {
+        var machine = Construct(true, false).Machine;
+        var output = machine.Run();
+
+        Assert.True(output[0] == 0.5);
+    }
+    [Fact]
+    public void False_and_false_gives_false()
+    {
+        var machine = Construct(false, false).Machine;
+        var output = machine.Run();
+
+        Assert.True(output[0] == 0);
+    }
+}
