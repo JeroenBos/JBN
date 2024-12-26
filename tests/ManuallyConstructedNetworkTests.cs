454c4a2ca181fbbb01a1c439ccc566635fc4dfb2
Jeroen Bos <15310631+JeroenBos@users.noreply.github.com>
2024-12-26 14:24:44 +0100

    wip

--- tests/ManuallyConstructedNetworkTests.cs
+++ tests/ManuallyConstructedNetworkTests.cs
@@ -48,16 +48,35 @@ public class AND
         return factory.Create(maxTime);
     }
     /// <summary>
-    ///     I ━━━(N)     // schema for this test: one an input axon connected to one neuron
+    /// Schema:
+    ///     I ━━━(N0)━━━━━━━┓              
+    ///                    (N2)
+    ///     I ━━━(N1)━━━━━━━┛
     /// 
-    /// t↓  I     N1     // means that we have Input (I) firings, and one neuron (N1)
-    ///           ┊      // means that we didn't receive charge from the previous step (as there was no previous step)
-    /// 0   + ━━━(1→0)   // the + means that the input axon fired. Then the (1) means the cumulative incoming charge results, and →0 means the charge after firing is 0.
-    ///           │      // means the neuron fired its axon (I propose ┈ and ┊ for "not fired")
-    /// 1   + ━━━(2→0)   // means that the Input axon fired, and the (2) is the sum of the previous charge (0, because it fired) and the two incoming axons
-    ///           │
-    /// 2        (1)      // means the output at time=2 was positive
+    /// t↓  I     N0        N1         N2     // means that we have Input (I) firings, and one neuron (N1)
+    ///          (0)       (0)        (0)     // initial charges
+    ///           ┊         ┊          ┊      // means that we didn't receive charge from the previous step (as there was no previous step)
+    /// 0   + ━━━(1)  + ━━━(1)      ┈┈(0)     // the + means that the input axon fired. Then the (1) means the cumulative incoming charge results, and →0 means the charge after firing is 0.
+    ///           │                           // means the neuron fired its axon (I propose ┈ and ┊ for "not fired")
+    /// 1   + ━━━(2→0)                        // means that the Input axon fired, and the (2) is the sum of the previous charge (0, because it fired) and the two incoming axons
+    ///           │                    
+    /// 2        (1)                          // means the output at time=2 was positive
     /// 
+    /// 
+    ///     I ━━━(0→1)┈┈┈┈┈┈┈              
+    ///                      (0)
+    ///     I ━━━(0→1)┈┈┈┈┈┈┈
+    ///
+    ///     I1        N0    N1    N2
+    /// t↓            0     0     0           // charge at t=0
+    ///     →0 →1                             // axons that deliver
+    ///               1*    1*    0           // charge at end of t=0. * indicates which fire
+    /// 1             0     0     0           // charge at t=1
+    ///     →0 →1     →2    →2                // axons that deliver
+    ///               1*    1*    2*          // charge after delivery
+    /// 2                                     // not relevant to test anymore
+
+    
     /// https://en.wikipedia.org/wiki/List_of_Unicode_characters#Box_Drawing
     /// </summary>
     [Fact]
