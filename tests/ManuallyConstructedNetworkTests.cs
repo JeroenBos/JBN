8c1b723cf7d8da19f2beb827c8b210506f9e3c7e
Jeroen Bos <15310631+JeroenBos@users.noreply.github.com>
2024-12-27 00:08:47 +0100

    Improve comment

--- tests/ManuallyConstructedNetworkTests.cs
+++ tests/ManuallyConstructedNetworkTests.cs
@@ -4,7 +4,7 @@ using System.Diagnostics;
 
 namespace Tests.JBSnorro.NN;
 
-/// https://en.wikipedia.org/wiki/List_of_Unicode_characters#Box_Drawing
+// https://en.wikipedia.org/wiki/List_of_Unicode_characters#Box_Drawing
 
 public class AND
 {
