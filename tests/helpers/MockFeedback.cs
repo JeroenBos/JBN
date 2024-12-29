0d15d30d217e53d4d5b19ccf9df0375af24076ff
Jeroen Bos <15310631+JeroenBos@users.noreply.github.com>
2024-12-29 01:02:56 +0100

    Create another test that also proves the principle

--- tests/helpers/MockFeedback.cs
+++ tests/helpers/MockFeedback.cs
@@ -4,3 +4,10 @@ class MockFeedback : IFeedback
 {
     public static MockFeedback Instance { get; } = new MockFeedback();
 }
+
+sealed class DirectionalFeedback : IFeedback
+{
+    private DirectionalFeedback() { }
+    public static DirectionalFeedback TooLow { get; } = new();
+    public static DirectionalFeedback TooHigh { get; } = new();
+}
