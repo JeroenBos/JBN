c579e7e1c54aea26f0be4d5eb9e1882fb7ac21c7
Jeroen Bos <15310631+JeroenBos@users.noreply.github.com>
2025-02-02 11:47:49 +0100

    Remove unused code + a rename

--- Extensions.cs
+++ Extensions.cs
@@ -5,31 +5,7 @@ using System.Runtime.CompilerServices;
 
 public static class Extensions
 {
-    public static HashSet<T?> Unique<T>(this T?[,] array)
-    {
-        var result = new HashSet<T?>();
-        for (int i = array.GetLowerBound(0); i <= array.GetUpperBound(0); i++)
-        {
-            for (int j = array.GetLowerBound(1); j <= array.GetUpperBound(1); j++)
-            {
-                result.Add(array[i, j]);
-            }
-        }
-        return result;
-    }
-    public static bool All<T>(this T[,] array, Func<T, bool> predicate)
-    {
-        for (int i = array.GetLowerBound(0); i <= array.GetUpperBound(0); i++)
-        {
-            for (int j = array.GetLowerBound(1); j <= array.GetUpperBound(1); j++)
-            {
-                if (!predicate(array[i, j]))
-                    return false;
-            }
-        }
-        return true;
-    }
-    public static IEnumerable<T> Scan<T>(this IEnumerable<T> sequence, Func<T, T, T> apply, T initial = default!)
+    public static IEnumerable<T> Fold<T>(this IEnumerable<T> sequence, Func<T, T, T> apply, T initial = default!)
     {
         T current = initial;
         foreach (T element in sequence)
@@ -37,18 +13,6 @@ public static class Extensions
             current = apply(current, element);
             yield return current;
         }
-    }
-    public static T[,] Initialize2DArray<T>(int size0, int size1, T initialValue)
-    {
-        var result = new T[size0, size1];
-        for (int y = 0; y < size1; y++)
-        {
-            for (int x = 0; x < size0; x++)
-            {
-                result[x, y] = initialValue;
-            }
-        }
-        return result;
     }
 }
 
